4ab2fd13b80caefeca4f85b650f745f6eef39d60
Emil Nymann <emilnymann96@gmail.com>
2020-01-10 22:33:22 +0100

    Added basic (incomplete) pathfanding for the zombie

--- scripts/actors/Zombie.gd
+++ scripts/actors/Zombie.gd
@@ -5,9 +5,23 @@ var health = 100
 onready var blood = load("res://entities/fx/BloodSpatter.tscn")
 onready var body = $Body
 
+var path : = PoolVector2Array()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+	
+func _process(delta):
+	var distance_to_walk = MOVE_SPEED * delta
+	
+	while distance_to_walk > 0 and path.size() > 0:
+		var distance_to_next_point = position.distance_to(path[0])
+		if distance_to_walk <= distance_to_next_point:
+			position += position.direction_to(path[0]) * distance_to_walk
+		else:
+			position = path[0]
+			path.remove(0)
+		distance_to_walk -= distance_to_next_point
 
 func _physics_process(delta):
 	if health <= 0:
