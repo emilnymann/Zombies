08eea454aed6ce9a55a1453d7245e9871d60bf59
Emil Nymann <emilnymann96@gmail.com>
2020-01-15 22:44:23 +0100

    Added zombie pathfinding nodes to the first level

--- scripts/levels/LevelLabEntrance01.gd
+++ scripts/levels/LevelLabEntrance01.gd
@@ -3,11 +3,19 @@ extends Node2D
 onready var player = $Player
 onready var player_camera = $PlayerCamera
 onready var hud = $HUD
+onready var pathfinding = $Pathfinding
+onready var pathfinding_timer = $PathfindingRefreshTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	var tree = get_tree()
+		
 	player_camera.attach_to(player)
 	hud.set_player(player)
+	tree.call_group("zombie", "set_player", player)
+	tree.call_group("zombie", "set_nav", pathfinding)
+	tree.call_group("zombie", "set_pathfinding_timer", pathfinding_timer)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
