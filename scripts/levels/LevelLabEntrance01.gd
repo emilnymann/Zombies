95f8aa14ceb9129be521bf8173f62efa9be2351d
Emil Nymann <emilnymann96@gmail.com>
2020-01-15 21:59:39 +0100

    Added dynamic functionality for the player camera to be attached to any node
    This will make it more easily reusable across levels

--- scripts/levels/LevelLabEntrance01.gd
+++ scripts/levels/LevelLabEntrance01.gd
@@ -1,12 +1,11 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var player = $Player
+onready var player_camera = $PlayerCamera
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player_camera.attach_to(player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- scripts/misc/PlayerCamera.gd
+++ scripts/misc/PlayerCamera.gd
@@ -1,9 +1,18 @@
 extends Camera2D
 
+onready var attached_to = $"."
+
 func _ready():
 	pass
 
 # warning-ignore:unused_argument
 func _process(delta):
+	
+	# camera follows the entity that it has been attached to
+	global_position = attached_to.global_position
+	
 	# smooth camera between player position and mouse position * strength factor
 	offset = ((( get_global_mouse_position() + global_position ) / 2 ) - global_position ) * 0.5
+
+func attach_to(node):
+	attached_to = node
