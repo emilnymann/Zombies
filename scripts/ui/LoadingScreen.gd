99db79e32c5c68e2a1777a86cb7f5a452bc08581
Emil Nymann <emilnymann96@gmail.com>
2020-01-18 14:40:44 +0100

    Added a timer to the loading screen so loading doesn't begin before the game is ready

--- scripts/ui/LoadingScreen.gd
+++ scripts/ui/LoadingScreen.gd
@@ -2,34 +2,39 @@ extends CanvasLayer
 
 onready var loading_bar = $LoadingVBoxContainer/LoadingBar
 onready var loading_label = $LoadingVBoxContainer/LoadingLabel
-onready var progress_tween = $ProgressTween
+onready var ready_timer = $ReadyTimer
 
 var loader : ResourceInteractiveLoader
 var loaded = false
+var ready = false
 var level_to_load : String
 
 func _ready():
-	loader = ResourceLoader.load_interactive("res://entities/levels/LevelLabEntrance01.tscn")
+	loader = ResourceLoader.load_interactive(level_to_load)
 
 func _process(delta):
-	if !loaded:
-		var err = loader.poll()
+	if ready:
+		if !loaded:
+			var err = loader.poll()
 	
-		if err == ERR_FILE_EOF:
-			loaded = true
-		elif err == OK:
-			update_progress()
+			if err == ERR_FILE_EOF:
+				loaded = true
+			elif err == OK:
+				update_progress()
+			else:
+				print("there was an error")
 		else:
-			print("there was an error")
-	else:
-		finish_loading()
+			finish_loading()
 
 func update_progress():
 	var progress : float = float(loader.get_stage()) / float(loader.get_stage_count()) * 100
+	print(progress)
 	
-	progress_tween.interpolate_property(loading_bar, "value", loading_bar.value, progress, 0.05, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
-	progress_tween.start()
+	loading_bar.value = progress
 
 func finish_loading():
 	loading_bar.value = 100
 	loading_label.text = "Press SPACE to continue..."
+
+func _on_ReadyTimer_timeout():
+	ready = true
