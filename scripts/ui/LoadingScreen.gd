61135ff3d17cd1dc696661cc603eab1a95121b57
Emil Nymann <emilnymann96@gmail.com>
2020-01-18 15:44:24 +0100

    Added loading screen label color tweening

--- scripts/ui/LoadingScreen.gd
+++ scripts/ui/LoadingScreen.gd
@@ -2,9 +2,12 @@ extends CanvasLayer
 
 onready var loading_bar = $LoadingVBoxContainer/LoadingBar
 onready var loading_label = $LoadingVBoxContainer/LoadingLabel
+onready var flash_tween = $FlashTween
+onready var flash_tween_2 = $FlashTween2
 
 var loader : ResourceInteractiveLoader
 var loaded = false
+var completed = false
 var ready = false
 var level : PackedScene
 var level_to_load : String
@@ -17,6 +20,7 @@ func _ready():
 	loading_label.text = "Loading " + level_display_name + "..."
 
 func _process(delta):
+	print(loading_label.get("custom_colors/font_color"))
 	if ready:
 		if !loaded:
 			var err = loader.poll()
@@ -28,7 +32,9 @@ func _process(delta):
 			else:
 				print("there was an error")
 		else:
-			finish_loading()
+			if !completed:
+				finish_loading()
+				
 			if Input.is_action_just_pressed("loading_continue"):
 				emit_signal("continue_pressed")
 
@@ -39,9 +45,22 @@ func update_progress():
 	loading_bar.value = progress
 
 func finish_loading():
+	completed = true
 	loading_bar.value = 100
 	level = loader.get_resource()
 	loading_label.text = "Press SPACE to continue..."
+	flash_tween.interpolate_property(loading_label, "custom_colors/font_color", Color(1, 1, 1, 1), Color(1, 1, 1, 0.2), 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+	flash_tween.start()
 
 func _on_ReadyTimer_timeout():
 	ready = true
+
+
+func _on_FlashTween_tween_all_completed():
+	flash_tween_2.interpolate_property(loading_label, "custom_colors/font_color", Color(1, 1, 1, 0.2), Color(1, 1, 1, 1), 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+	flash_tween_2.start()
+
+
+func _on_FlashTween2_tween_all_completed():
+	flash_tween.interpolate_property(loading_label, "custom_colors/font_color", Color(1, 1, 1, 1), Color(1, 1, 1, 0.2), 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+	flash_tween.start()
