4695365a3b5105cb2783d40d3c274422d176b38e
Emil Nymann <emilnymann96@gmail.com>
2020-01-18 14:27:52 +0100

    Added a tween to the loading screen progress bar to make it transition more smoothly and made the first level to load an export variable

--- scripts/misc/Global.gd
+++ scripts/misc/Global.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
-func _ready():
-	pass
+onready var main_menu = $MainMenu
+onready var loading_screen = load("res://entities/UI/LoadingScreen.tscn")
+export var level_path : String
 
-func _process(delta):
-	pass
+func _ready():
+	main_menu.get_node("MenuContainer/ButtonsContainer/PlayButton").connect("pressed", self, "_on_PlayButton_pressed")
+	main_menu.get_node("MenuContainer/ButtonsContainer/QuitButton").connect("pressed", self, "_on_QuitButton_pressed")
+
+func _on_PlayButton_pressed():
+	remove_child(main_menu)
+	loading_screen = loading_screen.instance()
+	loading_screen.level_to_load = level_path
+	add_child(loading_screen)
+	
+func _on_QuitButton_pressed():
+	get_tree().quit()
--- scripts/ui/LoadingScreen.gd
+++ scripts/ui/LoadingScreen.gd
@@ -2,9 +2,11 @@ extends CanvasLayer
 
 onready var loading_bar = $LoadingVBoxContainer/LoadingBar
 onready var loading_label = $LoadingVBoxContainer/LoadingLabel
+onready var progress_tween = $ProgressTween
 
 var loader : ResourceInteractiveLoader
 var loaded = false
+var level_to_load : String
 
 func _ready():
 	loader = ResourceLoader.load_interactive("res://entities/levels/LevelLabEntrance01.tscn")
@@ -24,7 +26,9 @@ func _process(delta):
 
 func update_progress():
 	var progress : float = float(loader.get_stage()) / float(loader.get_stage_count()) * 100
-	loading_bar.value = progress
+	
+	progress_tween.interpolate_property(loading_bar, "value", loading_bar.value, progress, 0.05, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+	progress_tween.start()
 
 func finish_loading():
 	loading_bar.value = 100
